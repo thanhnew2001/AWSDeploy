27fed6cc087828a1cc63c892b790143ad3e2ea89
thanhnew2001 <thanh.hispvietnam@gmail.com>
2022-08-22 14:43:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,47 @@
-const app = require('express')()
- 
-const mongoose = require('mongoose')
- 
-const bodyParser = require('body-parser')
- 
+var app = require('express')()
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
 app.use(bodyParser.json())
-
-const url = "mongodb+srv://thanh:thanh@cluster0.ntvke.mongodb.net/mydb?retryWrites=true&w=majority"
-
-const cors = require('cors')
-
-app.use(cors())
-
+ 
+const url = "mongodb+srv://admin:admin@cluster0.mnwyjpj.mongodb.net/db1?retryWrites=true&w=majority"
 mongoose.connect(url)
-
-
+ 
 //define a "table" structure
 var StudentSchema = new mongoose.Schema({
-    name: String
- })
-
+   id: String,
+   name: String
+})
+//create a model Student ==> students (database collection)
+//Teacher => teachers , Course => courses
+var Student = mongoose.model('Student', StudentSchema)
  
- var StudentModel = mongoose.model('Student', StudentSchema)  //name of collection
-
- app.get('/students', function(req, res){
-    StudentModel.find({}, function(err, students){
-        res.send(students)
-    })
- })
-
-  
+app.get('/students', function(req, res){
+   Student.find({}, function(err, students){
+       res.send(students)
+   })
+})
+ 
 app.post('/students', function(req, res){
-    StudentModel.create(req.body, function(err, student){
-        res.send(student)
-    })
- }) 
- 
-
- app.delete('/students/:_id', function(req, res){
-    StudentModel.deleteOne({_id: req.params._id}, function(err, result){
-        res.send(result)
-    })
- })
-  
+   Student.create(req.body, function(err, student){
+       res.send(student)
+   })
+})
+ app.delete('/students/:id', function(req, res){
+   Student.deleteOne({_id: req.params.id}, function(err, result){
+       res.send(result)
+   })
+})
  app.put('/students', function(req, res){
-    StudentModel.findOneAndUpdate({_id: req.body._id},{ name: req.body.name}, function(err, result){
-        res.send(result)
-    })
- })
-  
-  
- app.get('/students/search/:keyword', function(req, res){
-    StudentModel.find({name: req.params.keyword}, function(err, result){
-        res.send(result)
-    })
- })
+   Student.findOneAndUpdate({_id: req.body.id},{ name: req.body.name}, function(err, result){
+       res.send(result)
+   })
+})
  
-
- app.listen(4001)
-  
+app.get('/students/search/:keyword', function(req, res){
+   Student.find({name: req.params.keyword}, function(err, result){
+       res.send(result)
+   })
+})
  
-
-
-
+ 
+app.listen(9000)
